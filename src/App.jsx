60127b3af996c5e767a181fe53bd5ae370b9f7d6
JMDevelopmentWork <joaomatos.developmentwork@gmail.com>
2023-03-11 15:07:56 +0000

    added the active states

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,23 @@ import dataInfo from "./Data/Data";
 import {BiDotsHorizontalRounded} from 'react-icons/bi'
 
 function App() {
-  const [count, setCount] = useState(0);
+  
   const [daily, setDaily] = useState(true);
   const [weekly, setWeekly] = useState(false);
   const [monthly, setMonthly] = useState(false);
+  const [activeCard, setActiveCard] = useState(null);
+  const [activeIcon, setActiveIcon] = useState(null);
+
+  const handleCardMouseDown = (index) => {
+    setActiveCard(index);
+    setActiveIcon(null);
+  };
+
+  const handleIconMouseDown = (index, e) => {
+    e.stopPropagation();
+    setActiveIcon(index);
+    setActiveCard(null);
+  };
 
   const handleDaily = () => {
     setDaily(true);
@@ -59,42 +72,43 @@ function App() {
       </div>
 
       <div className="app__dashboard">
-        {dataInfo.map((item) => (
-          <div className="app__dashboard-card">
-            <div className={`app__dashboard-card-header ${item.style.toLowerCase()}`}>
-              
-              <img
-                className="app__dashboard-card-header-img"
-                src={item.image}
-                alt="svg"
-              />
-            </div>
-            <div className="app__dashboard-card-content">
-              <p className="app__dashboard-card-content-title">{item.title} <BiDotsHorizontalRounded className="icon"/></p>
-              {daily && (
-                <div className="app__dashboard-card-info">
-                  <p className="app__dashboard-card-content-current">{item.timeframes.daily.current}hrs</p>
-                  <p className="app__dashboard-card-content-previous">Yesterday - {item.timeframes.daily.previous}hrs</p>
-                </div>
-              )}
-
-              {weekly && (
-                <div>
-                  <p className="app__dashboard-card-content-current">{item.timeframes.weekly.current}hrs</p>
-                  <p className="app__dashboard-card-content-previous">Last Week - {item.timeframes.weekly.previous}hrs</p>
-                </div>
-              )}
-
-              {monthly && (
-                <div>
-                  <p className="app__dashboard-card-content-current">{item.timeframes.monthly.current}hrs</p>
-                  <p className="app__dashboard-card-content-previous">Last Month - {item.timeframes.monthly.previous}hrs</p>
-                </div>
-              )}
-            </div>
+      {dataInfo.map((item, index) => (
+        <div className="app__dashboard-card ">
+          <div className={`app__dashboard-card-header ${item.style.toLowerCase()}`}>
+            <img
+              className="app__dashboard-card-header-img"
+              src={item.image}
+              alt="svg"
+            />
           </div>
-        ))}
-      </div>
+          <div className={`app__dashboard-card-content ${index === activeCard ? 'activebg' : ''}`}           onMouseDown={() => handleCardMouseDown(index)}
+          onMouseUp={() => setActiveCard(null)}>
+              <p className="app__dashboard-card-content-title">{item.title} <BiDotsHorizontalRounded className={`icon ${index === activeIcon ? 'active' : ''}`}            onMouseDown={(e) => handleIconMouseDown(index, e)}
+            onMouseUp={() => setActiveIcon(null)}/></p>
+            {daily && (
+              <div className="app__dashboard-card-info">
+                <p className="app__dashboard-card-content-current">{item.timeframes.daily.current}hrs</p>
+                <p className="app__dashboard-card-content-previous">Yesterday - {item.timeframes.daily.previous}hrs</p>
+              </div>
+            )}
+
+            {weekly && (
+              <div>
+                <p className="app__dashboard-card-content-current">{item.timeframes.weekly.current}hrs</p>
+                <p className="app__dashboard-card-content-previous">Last Week - {item.timeframes.weekly.previous}hrs</p>
+              </div>
+            )}
+
+            {monthly && (
+              <div>
+                <p className="app__dashboard-card-content-current">{item.timeframes.monthly.current}hrs</p>
+                <p className="app__dashboard-card-content-previous">Last Month - {item.timeframes.monthly.previous}hrs</p>
+              </div>
+            )}
+          </div>
+        </div>
+      ))}
+    </div>
     </div>
     <div class="attribution">
     Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
